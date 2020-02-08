d5f2f8ab15018c22f64ffb92e09a9247abf5d23d
Alec Down <alecdown@gmail.com>
2020-02-08 09:24:40 -0600

    added jumbotron, enlarged text, fixed the button

---
+++ students.js
@@ -0,0 +1,110 @@
+const students = [
+    {
+        firstName: 'Tristan',
+        lastName: 'Jackson'
+    },
+    {
+        firstName: 'Andrea',
+        lastName: 'Fenderson'
+    },
+    {
+        firstName: 'Annaruth',
+        lastName: 'Mcbride'
+    },
+    {
+        firstName: 'Charlenne',
+        lastName: 'Alexander'
+    },
+    {
+        firstName: 'Garrett',
+        lastName: 'Griffey'
+    },
+    {
+        firstName: 'Greg',
+        lastName: 'Clark'
+    },
+    {
+        firstName: 'Iean',
+        lastName: 'Hennig'
+    },
+    {
+        firstName: 'James',
+        lastName: 'Malone'
+    },
+    {
+        firstName: 'Jay',
+        lastName: 'Tucker'
+    },
+    {
+        firstName: 'Jeffrey',
+        lastName: 'McFarland'
+    },
+    {
+        firstName: 'John',
+        lastName: 'Slota'
+    },
+    {
+        firstName: 'Joshua',
+        lastName: 'Everett'
+    },
+    {
+        firstName: 'Karla',
+        lastName: 'Dayhoff'
+    },
+    {
+        firstName: 'Kiara',
+        lastName: 'Mahan'
+    },
+    {
+        firstName: 'Kristin',
+        lastName: 'Centers'
+    },
+    {
+        firstName: 'Matt',
+        lastName: 'Turner'
+    },
+    {
+        firstName: 'Nari',
+        lastName: 'Louis'
+    },
+    {
+        firstName: 'Tro',
+        lastName: 'Monsivais'
+    },
+    {
+        firstName: 'Patrick',
+        lastName: 'Bridwell'
+    },
+    {
+        firstName: 'Piper',
+        lastName: 'Pearson'
+    },
+    {
+        firstName: 'Robert',
+        lastName: 'Williams'
+    },
+    {
+        firstName: 'Roger',
+        lastName: 'Jean-Charles'
+    },
+    {
+        firstName: 'Ryan',
+        lastName: 'Wilkinson'
+    },
+    {
+        firstName: 'Sammantha',
+        lastName: 'Sasenick'
+    },
+    {
+        firstName: 'Theresa',
+        lastName: 'Eatherly'
+    },
+    {
+        firstName: 'Spencer',
+        lastName: 'Lambert'
+    },
+    {
+        firstName: 'Timothy',
+        lastName: 'Mickiewicz'
+    }
+];
