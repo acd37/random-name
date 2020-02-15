a41029d552d31c2be0dc83718684951b73044006
Alec Down <alecdown@gmail.com>
2020-02-15 08:59:20 -0600

    enables splice of selected student and init()

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,30 @@ studentScript.src = './assets/js/students.js';
 document.body.appendChild(studentScript);
 
 function getRandomStudents() {
-    const random = Math.floor(Math.random() * students.length);
+    if (students.length > 0) {
+        const random = Math.floor(Math.random() * students.length);
 
-    $('#random-name').html(
-        '<p>' + students[random].firstName + ' ' + students[random].lastName
-    );
+        $('#random-name').html(
+            '<p>' +
+                students[random].firstName +
+                ' ' +
+                students[random].lastName +
+                '</p>'
+        );
+
+        students.splice(random, 1);
+
+        console.log(students);
+    } else {
+        const init = confirm(
+            "You're out of students. Hit okay to re-populate."
+        );
+
+        console.log(init);
+        if (init) {
+            initStudents();
+        }
+    }
 }
 
 $('#randomize').click(function() {
--- assets/js/students.js
+++ assets/js/students.js
@@ -1,136 +1,112 @@
-const students = [
-    {
-        firstName: 'Tristan',
-        lastName: 'Jackson'
-    },
-    {
-        firstName: 'Andrea',
-        lastName: 'Fenderson'
-    },
-    {
-        firstName: 'Annaruth',
-        lastName: 'McBride'
-    },
-    {
-        firstName: 'Charlenne',
-        lastName: 'Alexander'
-    },
-    {
-        firstName: 'Garrett',
-        lastName: 'Griffey'
-    },
-    {
-        firstName: 'Greg',
-        lastName: 'Clark'
-    },
-    {
-        firstName: 'Iean',
-        lastName: 'Hennig'
-    },
-    {
-        firstName: 'James',
-        lastName: 'Malone'
-    },
-    {
-        firstName: 'Jay',
-        lastName: 'Tucker'
-    },
-    {
-        firstName: 'Jeffrey',
-        lastName: 'McFarland'
-    },
-    {
-        firstName: 'John',
-        lastName: 'Slota'
-    },
-    {
-        firstName: 'Joshua',
-        lastName: 'Everett'
-    },
-    {
-        firstName: 'Karla',
-        lastName: 'Dayhoff'
-    },
-    {
-        firstName: 'Kiara',
-        lastName: 'Mahan'
-    },
-    {
-        firstName: 'Kristin',
-        lastName: 'Centers'
-    },
-    {
-        firstName: 'Matt',
-        lastName: 'Turner'
-    },
-    {
-        firstName: 'Nari',
-        lastName: 'Louis'
-    },
-    {
-        firstName: 'Tro',
-        lastName: 'Monsivais'
-    },
-    {
-        firstName: 'Patrick',
-        lastName: 'Bridwell'
-    },
-    {
-        firstName: 'Piper',
-        lastName: 'Pearson'
-    },
-    {
-        firstName: 'Robert',
-        lastName: 'Williams'
-    },
-    {
-        firstName: 'Roger',
-        lastName: 'Jean-Charles'
-    },
-    {
-        firstName: 'Ryan',
-        lastName: 'Wilkinson'
-    },
-    {
-        firstName: 'Sammantha',
-        lastName: 'Sasenick'
-    },
-    {
-        firstName: 'Theresa',
-        lastName: 'Eatherly'
-    },
-    {
-        firstName: 'Spencer',
-        lastName: 'Lambert'
-    },
-    {
-        firstName: 'Timothy',
-        lastName: 'Mickiewicz'
-    }
-];
+let students = [];
 
-const adjectives = [
-    'moldy',
-    'frosty',
-    'overbaked',
-    'toasted',
-    'burned',
-    'stinky',
-    'steaming hot',
-    'neglected',
-    'deflated',
-    'crumbled'
-];
+function initStudents() {
+    students.push(
+        {
+            firstName: 'Andrea',
+            lastName: 'Fenderson'
+        },
+        {
+            firstName: 'Annaruth',
+            lastName: 'McBride'
+        },
+        {
+            firstName: 'Charlenne',
+            lastName: 'Alexander'
+        },
+        {
+            firstName: 'Garrett',
+            lastName: 'Griffey'
+        },
+        {
+            firstName: 'Greg',
+            lastName: 'Clark'
+        },
+        {
+            firstName: 'Iean',
+            lastName: 'Hennig'
+        },
+        {
+            firstName: 'James',
+            lastName: 'Malone'
+        },
+        {
+            firstName: 'Jay',
+            lastName: 'Tucker'
+        },
+        {
+            firstName: 'Jeffrey',
+            lastName: 'McFarland'
+        },
+        {
+            firstName: 'John',
+            lastName: 'Slota'
+        },
+        {
+            firstName: 'Joshua',
+            lastName: 'Everett'
+        },
+        {
+            firstName: 'Karla',
+            lastName: 'Dayhoff'
+        },
+        {
+            firstName: 'Kiara',
+            lastName: 'Mahan'
+        },
+        {
+            firstName: 'Kristin',
+            lastName: 'Centers'
+        },
+        {
+            firstName: 'Matt',
+            lastName: 'Turner'
+        },
+        {
+            firstName: 'Nari',
+            lastName: 'Louis'
+        },
+        {
+            firstName: 'Tro',
+            lastName: 'Monsivais'
+        },
+        {
+            firstName: 'Patrick',
+            lastName: 'Bridwell'
+        },
+        {
+            firstName: 'Piper',
+            lastName: 'Pearson'
+        },
+        {
+            firstName: 'Robert',
+            lastName: 'Williams'
+        },
+        {
+            firstName: 'Roger',
+            lastName: 'Jean-Charles'
+        },
+        {
+            firstName: 'Ryan',
+            lastName: 'Wilkinson'
+        },
+        {
+            firstName: 'Sammantha',
+            lastName: 'Sasenick'
+        },
+        {
+            firstName: 'Theresa',
+            lastName: 'Eatherly'
+        },
+        {
+            firstName: 'Spencer',
+            lastName: 'Lambert'
+        },
+        {
+            firstName: 'Timothy',
+            lastName: 'Mickiewicz'
+        }
+    );
+}
 
-const bakedGoods = [
-    'cupcake',
-    'cookie',
-    'muffin',
-    'cake',
-    'lemon loaf',
-    'brownie',
-    'chocolate éclair',
-    'macaron',
-    'donut',
-    'pie'
-];
+initStudents();
